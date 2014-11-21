3ddebf2acf598ad871fce7f616c7c9d1bbdfb847
Alexandre Brasil <alexandre.brasil@gmail.com>
2014-11-21 03:31:42 -0200

    Adding new cards
    Form for adding a new card into a lane with crude validation.

--- src/main/webapp/js/boards.js
+++ src/main/webapp/js/boards.js
@@ -5,24 +5,37 @@ boardsModule.service('Boards', [function() {
 	              	{
 						name: 'Yaks Development', 
 						lanes: [
-							{name: 'Unprioritized', cards:[]}, 
+							{name: 'Unprioritized', cards:[{name: 'Create card component', description: 'Reusable component to edit a card whenever needed.'}]}, 
 							{name: 'Pending', cards: []}, 
-							{name: 'Doing', cards:[]},
+							{name: 'Doing', cards:[{name: 'This card has a huge name that goes on and on and on. For real! No kidding!', description: 'Add cards to each lane when they are present.'},
+							                       {name: 'Add cards to lanes', description: 'Add cards to each lane when they are present.'},
+							                       {name: 'Add cards to lanes', description: 'Add cards to each lane when they are present.'},
+							                       {name: 'Add cards to lanes', description: 'Add cards to each lane when they are present really long text goes here anytime i want! Anything else?'},
+							                       {name: 'Add cards to lanes', description: 'Add cards to each lane when they are present.'},
+							                       {name: 'Add cards to lanes', description: 'Add cards to each lane when they are present.'},
+							                       {name: 'Add cards to lanes', description: 'Add cards to each lane when they are present.'},
+							                       {name: 'Add cards to lanes', description: 'Add cards to each lane when they are present.'},
+							                       {name: 'Add cards to lanes', description: 'Add cards to each lane when they are present.'}]},
 							{name: 'Done', cards: []},
 							{name: 'Deployed', cards: []}
 					]},
 					{
 						name: 'Cervejas',
 						lanes: [
-						    {name: 'Receitas', cards: []},
-						    {name: 'FermentaÃ§Ã£o', cards: []},
-						    {name: 'MaturaÃ§Ã£o', cards: []},
-						    {name: 'RefermentaÃ§Ã£o na garrafa longo longo', cards: []},
+						    {name: 'Receitas', cards: [{name: 'Bittervet', description: 'Malte, Água, lúpulo!'},
+						                               {name: 'HoneyWheat', description: 'Malte, malte mel, água, lúpulo.'}]},
+						    {name: 'Fermentação', cards: []},
+						    {name: 'Maturação', cards: []},
+						    {name: 'Refermentação na garrafa', cards: [{name: 'Bittervet', description: 'Meh...'}]},
 						    {name: 'Prontas e em estoque', cards: []},
 						    {name: 'Arquivo morto', cards:[]}
 					]}];
 	
 	this.list = function() {
 		return boards;
+	}
+	
+	this.addCard = function(lane, card) {
+		lane.cards.push(card);
 	}
 }])
--- src/main/webapp/js/yaks.js
+++ src/main/webapp/js/yaks.js
@@ -1,7 +1,34 @@
 var yaksApp = angular.module('yaks',['boards']);
 
-yaksApp.controller('MainController', ['$scope', 'Boards', function($scope, Boards) {
+yaksApp.controller('MainController', ['$scope', 'Boards', '$window', function($scope, Boards, $window) {
 	$scope.boards = Boards.list();
 	$scope.selectedBoard = $scope.boards[0];
+	$scope.showNewCard = false;
+	$scope.newCardName = "";
+	$scope.newCardText = "";
+	
+	$scope.addCard = function(lane) {
+		$scope.showNewCard = true;
+		$scope.selectedLane = lane;
+	}
+	
+	$scope.saveCard = function() {
+		if($scope.newCardName == null || $scope.newCardName.trim().length == 0 || $scope.newCardText == null || $scope.newCardText.trim().length == 0) {
+			$window.alert("Please inform the card name and a starting text before proceeding.");
+			return;
+		}
+		
+		var card = {
+						name: $scope.newCardName,
+						description: $scope.newCardText
+				   }
+		Boards.addCard($scope.selectedLane, card);
+		
+		$scope.showNewCard = false;
+		$scope.newCardName = '';
+		$scope.newCardText = '';
+		$scope.newCardForm.$setPristine(true);
+		$scope.newCardForm.$setUntouched(true);
+	}
 	
 }]);
